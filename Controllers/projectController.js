0929f128028e85aa368defb40105d4fd4daccdf2
Adithyan Anil <adithyananil2002@gmail.com>
2023-11-19 22:54:09 +0530

    v4

--- Controllers/projectController.js
+++ Controllers/projectController.js
@@ -1,8 +1,30 @@
-const projects =require('../Models/projectSchema')
+const projects = require("../Models/projectSchema")
 
-//add project 
 
-exports.addProject =(req,res)=> {
-    console.log("inside add project function");
-    res.status(200).json("add Project request recived!!!")
+
+// add projects
+
+
+
+exports.addProjects = async (req,res)=>{
+    console.log("inside Add project function");
+    const userId = req.payload
+    const thumbnail = req.file.filename
+    const {title,languages,overview,gitHub,website}= req.body
+    // console.log(`${title},${languages},${overview},${gitHub},${website}, ${userId}`);
+
+    try{
+        const existingProject = await projects.findOne({gitHub})
+        if(existingProject){
+            res.status(406).json("Projects already exist !! Upload another")
+        }else{
+            const newProject = new projects({
+                title,languages,overview,gitHub,website,thumbnail,userId
+            })
+            await newProject.save()
+            res.status(200).json(newProject)
+        }
+    }catch(err){
+        res.status(401).json(`Request failed, Error:${err}`)
+    }
 }
--- Routes/router.js
+++ Routes/router.js
@@ -1,13 +1,14 @@
-const express= require('express')
-const router = new express.Router()
-const userController =require('../Controllers/userController')
-const projectController =require('../Controllers/projectController')
-const jwtMiddleware = require('../Middlewares/jwtMiddleware')
+const express = require("express");
+const router = new express.Router();
+const userController = require("../Controllers/userController");
+const projectController = require("../Controllers/projectController");
+const jwtMiddleware = require("../Middlewares/jwtMiddleware");
+const multerConfig =require('../Middlewares/multerMiddleware')
 // register
-router.post('/user/register',userController.register)
-//login 
-router.post('/user/login',userController.login)
+router.post("/user/register", userController.register);
+//login
+router.post("/user/login", userController.login);
 //add project
-router.post('/project/add',jwtMiddleware,projectController.addProject)
+router.post("/project/add", jwtMiddleware,multerConfig.single('thumbnail'), projectController.addProjects);
 
-module.exports=router
+module.exports = router;
